4744049529d7d5d2504f4ab8073791778bc742fd
PXTN45 <120036577+PXTN45@users.noreply.github.com>
2023-01-04 11:11:18 +0700

    first commit

---
+++ Test Suites/TS2001.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS2001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3a09704e-fb9f-4f2d-869d-d23f1bd9913e</testSuiteGuid>
+</TestSuiteEntity>
